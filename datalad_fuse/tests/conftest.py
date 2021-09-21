5429f9e36990513192523845d610fed5b3438681
John T. Wodder II <git@varonathe.org>
2021-09-21 15:31:47 -0400

    Cut down on explicit test data

--- datalad_fuse/tests/conftest.py
+++ datalad_fuse/tests/conftest.py
@@ -14,6 +14,11 @@ DATA_DIR = Path(__file__).with_name("data")
 lgr = logging.getLogger("datalad_fuse.tests")
 
 
+@pytest.fixture(scope="session")
+def data_files():
+    return {p.name: p.read_bytes() for p in DATA_DIR.iterdir() if p.is_file()}
+
+
 def serve_path_via_http(hostname, path, queue):
     os.chdir(path)
     httpd = HTTPServer((hostname, 0), SimpleHTTPRequestHandler)
@@ -53,26 +58,25 @@ def local_server():
 
 
 @pytest.fixture(params=["remote", "local"])
-def url_dataset(local_server, request, tmp_path):
+def url_dataset(data_files, local_server, request, tmp_path):
     ds = Dataset(str(tmp_path)).create()
-    for p in DATA_DIR.iterdir():
-        if p.is_file():
-            if request.param == "remote":
-                ds.repo.add_url_to_file(
-                    p.name, f"{local_server}/{p.name}", options=["--relaxed"]
-                )
-            else:
-                ds.download_url(urls=f"{local_server}/{p.name}", path=p.name)
-            # Add an invalid URL in order to test bad-URL-fallback.
-            # Add it after `download_url()` is called in order to not cause an
-            #  error on filesystems without symlinks (in which case doing
-            #  `add_url_to_file()` would cause the file to be created as a
-            #  non-symlink, which `download_url()` would then see as the file
-            #  already being present, leading to an error).
-            # It appears that git annex returns files' URLs in lexicographic
-            #  order, so in order for the bad URL to be tried first, we insert
-            #  a '0'.
+    for fname in data_files:
+        if request.param == "remote":
             ds.repo.add_url_to_file(
-                p.name, f"{local_server}/0{p.name}", options=["--relaxed"]
+                fname, f"{local_server}/{fname}", options=["--relaxed"]
             )
+        else:
+            ds.download_url(urls=f"{local_server}/{fname}", path=fname)
+        # Add an invalid URL in order to test bad-URL-fallback.
+        # Add it after `download_url()` is called in order to not cause an
+        #  error on filesystems without symlinks (in which case doing
+        #  `add_url_to_file()` would cause the file to be created as a
+        #  non-symlink, which `download_url()` would then see as the file
+        #  already being present, leading to an error).
+        # It appears that git annex returns files' URLs in lexicographic
+        #  order, so in order for the bad URL to be tried first, we insert
+        #  a '0'.
+        ds.repo.add_url_to_file(
+            fname, f"{local_server}/0{fname}", options=["--relaxed"]
+        )
     return ds
--- datalad_fuse/tests/test_fsspec_head.py
+++ datalad_fuse/tests/test_fsspec_head.py
@@ -1,87 +1,63 @@
 # datalad.api needs to be imported in order for fsspec_head to be available
 import datalad.api  # noqa
 from datalad.tests.utils import assert_in_results
+from linesep import split_terminated
+import pytest
 
 
-def test_get_default_lines_text(url_dataset):
+def first_n_lines(blob, n):
+    # In order to match the behavior of Python's binary IO files, only \n
+    # should be treated as a line separator, so bytes.splitlines() is not an
+    # option.
+    return b"".join(split_terminated(blob, b"\n", True)[:n])
+
+
+def test_get_default_lines_text(data_files, url_dataset):
     assert_in_results(
         url_dataset.fsspec_head("text.txt"),
         action="fsspec-head",
         type="dataset",
         status="ok",
-        data=b"""\
-Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
-euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.  Ut wisi enim
-ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl
-ut aliquip ex ea commodo consequat.  Duis autem vel eum iriure dolor in
-hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu
-feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui
-blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla
-facilisi.  Nam liber tempor cum soluta nobis eleifend option congue nihil
-imperdiet doming id quod mazim placerat facer possim assum.  Typi non habent
-claritatem insitam; est usus legentis in iis qui facit eorum claritatem.
-""",
+        data=first_n_lines(data_files["text.txt"], 10),
     )
 
 
-def test_get_lines_text(url_dataset):
+@pytest.mark.parametrize("lines", [0, 4, 20])
+def test_get_lines_text(data_files, lines, url_dataset):
     assert_in_results(
-        url_dataset.fsspec_head("text.txt", lines=4),
+        url_dataset.fsspec_head("text.txt", lines=lines),
         action="fsspec-head",
         type="dataset",
         status="ok",
-        data=b"""\
-Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
-euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.  Ut wisi enim
-ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl
-ut aliquip ex ea commodo consequat.  Duis autem vel eum iriure dolor in
-""",
+        data=first_n_lines(data_files["text.txt"], lines),
     )
 
 
-def test_get_bytes_text(url_dataset):
+def test_get_bytes_text(data_files, url_dataset):
     assert_in_results(
         url_dataset.fsspec_head("text.txt", bytes=100),
         action="fsspec-head",
         type="dataset",
         status="ok",
-        data=b"""\
-Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
-euismod tincidunt ut""",
+        data=data_files["text.txt"][:100],
     )
 
 
-def test_get_lines_binary(url_dataset):
+def test_get_lines_binary(data_files, url_dataset):
     assert_in_results(
         url_dataset.fsspec_head("binary.png", lines=3),
         action="fsspec-head",
         type="dataset",
         status="ok",
-        data=bytes.fromhex(
-            "89 50 4e 47 0d 0a 1a 0a  00 00 00 0d 49 48 44 52"
-            "00 00 00 20 00 00 00 20  08 06 00 00 00 73 7a 7a"
-            "f4 00 00 00 19 74 45 58  74 53 6f 66 74 77 61 72"
-            "65 00 41 64 6f 62 65 20  49 6d 61 67 65 52 65 61"
-            "64 79 71 c9 65 3c 00 00  04 06 49 44 41 54 78 da"
-            "c4 57 5b 48 14 61 14 3e  a6 ae d1 78 99 15 d9 29"
-            "f3 b2 0a"
-        ),
+        data=first_n_lines(data_files["binary.png"], 3),
     )
 
 
-def test_get_bytes_binary(url_dataset):
+def test_get_bytes_binary(data_files, url_dataset):
     assert_in_results(
         url_dataset.fsspec_head("binary.png", bytes=100),
         action="fsspec-head",
         type="dataset",
         status="ok",
-        data=bytes.fromhex(
-            "89 50 4e 47 0d 0a 1a 0a  00 00 00 0d 49 48 44 52"
-            "00 00 00 20 00 00 00 20  08 06 00 00 00 73 7a 7a"
-            "f4 00 00 00 19 74 45 58  74 53 6f 66 74 77 61 72"
-            "65 00 41 64 6f 62 65 20  49 6d 61 67 65 52 65 61"
-            "64 79 71 c9 65 3c 00 00  04 06 49 44 41 54 78 da"
-            "c4 57 5b 48 14 61 14 3e  a6 ae d1 78 99 15 d9 29"
-            "f3 b2 0a 5e"
-        ),
+        data=data_files["binary.png"][:100],
     )
