2a08998b9b734a634310a6466b2669254d680f5d
John T. Wodder II <git@varonathe.org>
2021-10-18 09:18:03 -0400

    Get tests to pass

--- datalad_fuse/tests/test_fuse.py
+++ datalad_fuse/tests/test_fuse.py
@@ -1,18 +1,24 @@
 import subprocess
 
-from datalad.tests.utils import assert_in_results
 import pytest
 
 
 @pytest.mark.libfuse
 def test_fuse(data_files, tmp_path, url_dataset):
-    assert_in_results(
-        url_dataset.fusefs(str(tmp_path)),
-        action="fusefs",
-        type="dataset",
-        path=str(tmp_path),
-        status="ok",
+    p = subprocess.Popen(
+        ["datalad", "fusefs", "-d", url_dataset.path, "--foreground", str(tmp_path)]
+    )
+    # Check that the command didn't fail immediately:
+    with pytest.raises(subprocess.TimeoutExpired):
+        p.wait(timeout=3)
+    assert (
+        sorted(q.name for q in tmp_path.iterdir())
+        == [
+            ".datalad",
+            ".gitattributes",
+        ]
+        + sorted(data_files)
     )
     for fname, blob in data_files.items():
         assert (tmp_path / fname).read_bytes() == blob
-    subprocess.run(["fusermount", "-u", str(tmp_path)], check=True)
+    p.terminate()
