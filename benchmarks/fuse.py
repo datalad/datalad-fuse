23471d8fd9241ff27155c2080009f9179322c8ea
John T. Wodder II <git@varonathe.org>
2022-02-21 14:40:32 -0500

    Setup asv

---
+++ benchmarks/fuse.py
@@ -0,0 +1,50 @@
+from pathlib import Path
+import subprocess
+
+TEST_REPO = "https://github.com/dandisets/000007"
+TEST_TAG = "0.220126.1903"
+
+
+class FuseBenchmarks:
+    timeout = 3600
+
+    def setup_cache(self):
+        subprocess.run(
+            ["git", "clone", TEST_REPO, "work"],
+            check=True,
+        )
+        subprocess.run(["git", "checkout", TEST_TAG], cwd="work", check=True)
+        return Path("work").resolve()
+
+    def setup(self, work_dir):
+        self.mount = Path("mount")
+        self.mount.mkdir(exist_ok=True)
+        self.p = subprocess.Popen(
+            [
+                "datalad",
+                "fusefs",
+                "-d",
+                str(work_dir),
+                "--foreground",
+                str(self.mount),
+            ]
+        )
+        try:
+            self.p.wait(timeout=3)
+        except subprocess.TimeoutExpired:
+            pass
+        else:
+            raise RuntimeError("datalad fusefs died on startup!")
+
+    def time_ls(self, _work_dir):
+        subprocess.run(["ls", "-R"], cwd=self.mount, check=True)
+
+    def time_ls_l(self, _work_dir):
+        subprocess.run(["ls", "-lR"], cwd=self.mount, check=True)
+
+    def time_ls_lL(self, _work_dir):
+        subprocess.run(["ls", "-lLR"], cwd=self.mount, check=True)
+
+    def teardown(self, _work_dir):
+        self.p.terminate()
+        self.p.wait(3)
