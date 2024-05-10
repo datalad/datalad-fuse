808d184a182c66ef03f97b46955f7e93bcfeecbb
Horea Christian <chr@chymera.eu>
2024-05-10 04:58:33 -0400

    Set required git configuration for initiated test repository

--- datalad_fuse/tests/test_fsspec_head.py
+++ datalad_fuse/tests/test_fsspec_head.py
@@ -131,6 +131,8 @@ def test_git_repo(tmp_path):
     TEXT = (Path(__file__).with_name("data") / "text.txt").read_bytes()
     (tmp_path / "text.txt").write_bytes(TEXT)
     subprocess.run(["git", "add", "text.txt"], cwd=tmp_path, check=True)
+    subprocess.run(["git", "config", "user.email", "place@holder.org"], cwd=tmp_path, check=True)
+    subprocess.run(["git", "config", "user.name", "Place Holder"], cwd=tmp_path, check=True)
     subprocess.run(["git", "commit", "-m", "Add a file"], cwd=tmp_path, check=True)
     ds = Dataset(tmp_path)
     assert_in_results(
